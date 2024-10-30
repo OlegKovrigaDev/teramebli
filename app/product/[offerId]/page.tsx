27aedd938b106f88e37b63078159e78f94e2728c
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-10-30 10:50:28 +0100

    ProductPage refactoring
    ReviewPopover, constant

--- app/product/[offerId]/page.tsx
+++ app/product/[offerId]/page.tsx
@@ -1,18 +1,12 @@
 'use client'
-import {
-	useAddProductReviewMutation,
-	useFetchReviewsByOfferIdQuery,
-} from '@/api/categoryApi'
+import { useFetchReviewsByOfferIdQuery } from '@/api/categoryApi'
 import { Loading } from '@/components/Loading'
 import { Accord } from '@/components/shared/accord'
 import { CrumbsLinks } from '@/components/shared/CrumbsLinks'
 import { Gallery } from '@/components/shared/gallery'
+import { ReviewPopover } from '@/components/shared/product/ReviewPopover'
 import {
 	Button,
-	Input,
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
 	Tabs,
 	TabsContent,
 	TabsList,
@@ -20,12 +14,10 @@ import {
 } from '@/components/ui'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Label } from '@/components/ui/label'
+import { CHARACTERISTICS } from '@/constants'
 import { useProductData } from '@/hooks'
 import { addToCart } from '@/store/cartSlice'
-import { Cross2Icon } from '@radix-ui/react-icons'
-import { PopoverClose } from '@radix-ui/react-popover'
 import Link from 'next/link'
-import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 export default function page({ params }: { params: { offerId: string } }) {
@@ -43,31 +35,11 @@ export default function page({ params }: { params: { offerId: string } }) {
 		subCategoryId,
 	} = useProductData(offerId)
 
-	const [name, setName] = useState('')
-	const [reviewText, setReviewText] = useState('')
-
-	const [addReview, { isLoading: isSubmitting, isSuccess }] =
-		useAddProductReviewMutation()
-
 	const {
 		data: reviews,
 		refetch: refetchReviews,
 		isFetching: isFetchingReviews,
 	} = useFetchReviewsByOfferIdQuery(offerId)
-
-	const handleAddReview = async () => {
-		if (name.trim() && reviewText.trim()) {
-			await addReview({
-				name,
-				review: reviewText,
-				createdAt: new Date(),
-				offerId,
-			})
-			setReviewText('')
-			setName('')
-			refetchReviews()
-		}
-	}
 
 	if (isLoading) return <Loading />
 	if (error)
@@ -126,8 +98,7 @@ export default function page({ params }: { params: { offerId: string } }) {
 							<div
 								dangerouslySetInnerHTML={{
 									__html:
-										product.params['Опис текст(сайт)'] ||
-										'Описание отсутствует',
+										product.params['Опис текст(сайт)'] || 'Опис відсутній',
 								}}
 							/>
 						</TabsContent>
@@ -135,107 +106,29 @@ export default function page({ params }: { params: { offerId: string } }) {
 							className='bg-white mt-4 py-4 px-2 rounded'
 							value='characteristics'
 						>
-							<div className='w-full p-4 rounded-lg '>
-								<div className='mb-6'>
-									<h3 className='text-lg font-semibold mb-2'>
-										Загальна інформація
-									</h3>
-									<div className='grid grid-cols-2 gap-4'>
-										<div className='text-gray-500'>Вид товару</div>
-										<div>{product.params['Приналежність до категорії']}</div>
-									</div>
-								</div>
-
-								<div className='mb-6'>
-									<h3 className='text-lg font-semibold mb-2'>
-										Габаритні розміри
-									</h3>
-									<div className='grid grid-cols-2 gap-4'>
-										<div className='text-gray-500'>Висота (см)</div>
-										<div>
-											{product.params['Габарит.розміри.Висота(см)(сайт)']}
+							<div className='w-full p-4 rounded-lg'>
+								<h3 className='text-lg font-semibold mb-4'>
+									Загальна інформація
+								</h3>
+								<div className='grid grid-cols-2 gap-4'>
+									{CHARACTERISTICS.map(({ label, key }) => (
+										<div key={key} className='grid grid-cols-2 gap-4'>
+											<div className='text-gray-500'>{label}</div>
+											<div>{product.params[key] ?? 'Не вказано'}</div>
 										</div>
-										<div className='text-gray-500'>Довжина(см)</div>
-										<div>
-											{product.params['Габарит.розміри.Довжина(см)(сайт)']}
-										</div>
-										<div className='text-gray-500'>Ширина (см)</div>
-										<div>
-											{product.params['Габарит.розміри.Ширина(см)(сайт)']}
-										</div>
-										<div className='text-gray-500'>Додатково ліжка</div>
-										<div>{product.params['Додатково ліжка(сайт ліжка)']}</div>
-										<div className='text-gray-500'>Матеріал ліжка</div>
-										<div>{product.params['Матеріал ліжка(сайт ліжка)']}</div>
-										<div className='text-gray-500'>Ніша для білизни</div>
-										<div>{product.params['Ніша для білизни(сайт ліжка)']}</div>
-										<div className='text-gray-500'>Основа під матрац</div>
-										<div>{product.params['Основа під матрац(сайт ліжка)']}</div>
-										<div className='text-gray-500'>Підйомний механізм</div>
-										<div>
-											{product.params['Підйомний механізм(сайт ліжка)']}
-										</div>
-										<div className='text-gray-500'>
-											Ширина спального місця (см)
-										</div>
-										<div>
-											{product.params['Роз.спал.місц.ширина(см)(сайт)']}
-										</div>
-										<div className='text-gray-500'>
-											Довжина спального місця (см)
-										</div>
-										<div>
-											{product.params['Розм.спал.місц.Довжина(см)(сайт)']}
-										</div>
-										<div className='text-gray-500'>Тип ліжка</div>
-										<div>{product.params['Тип ліжка(сайт ліжка)']}</div>
-									</div>
+									))}
 								</div>
 							</div>
 						</TabsContent>
+
 						<TabsContent
 							className='bg-white mt-4 py-4 px-2 rounded'
 							value='feedback'
 						>
-							<Popover>
-								<div className='flex justify-end'>
-									<PopoverTrigger className='rounded-xl border-2 border-gray text-gray py-2 px-5 mb-6'>
-										Залишити відгук
-									</PopoverTrigger>
-								</div>
-								<PopoverContent className='w-96 absolute -right-24'>
-									<Input
-										type='text'
-										placeholder="Ваше ім'я"
-										className='w-full p-2 mb-2 border rounded'
-										value={name}
-										onChange={e => setName(e.target.value)}
-									/>
-									<textarea
-										className='w-full p-2 mb-2 border rounded'
-										rows={4}
-										placeholder='Ваш відгук'
-										value={reviewText}
-										onChange={e => setReviewText(e.target.value)}
-									/>
-									<PopoverClose
-										onClick={handleAddReview}
-										className='mt-2 rounded-[8px] px-4 py-2 bg-gray text-white'
-										disabled={isSubmitting}
-										aria-label='Close'
-									>
-										{isSubmitting ? 'Надсилання...' : 'Додати відгук'}
-									</PopoverClose>
-									{/* <Button
-										onClick={handleAddReview}
-										className='mt-2 bg-gray text-white'
-										disabled={isSubmitting}
-									>
-										{isSubmitting ? 'Надсилання...' : 'Додати відгук'}
-									</Button> */}
-								</PopoverContent>
-							</Popover>
-
+							<ReviewPopover
+								offerId={offerId}
+								refetchReviews={refetchReviews}
+							/>
 							<div className='bg-white space-y-6'>
 								{isFetchingReviews ? (
 									<Loading />
