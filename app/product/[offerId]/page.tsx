0a39b1173e6879e0ef00e81c3b062eb51763a315
olegkovrigadev <41855503+OlegKovrigaDev@users.noreply.github.com>
2024-11-12 16:47:28 +0200

    Add product adaptive

--- app/product/[offerId]/page.tsx
+++ app/product/[offerId]/page.tsx
@@ -3,6 +3,7 @@ import { useFetchReviewsByOfferIdQuery } from '@/api/categoryApi'
 import { Loading } from '@/components/Loading'
 import { Accord } from '@/components/shared/accord'
 import { CrumbsLinks } from '@/components/shared/CrumbsLinks'
+import { Gallery } from '@/components/shared/gallery'
 import { ReviewPopover } from '@/components/shared/product/ReviewPopover'
 import {
 	Button,
@@ -91,8 +92,8 @@ export default function page({ params }: { params: { offerId: string } }) {
 			/>
 			<div className='flex flex-col xl:flex-row justify-between pb-16'>
 				<div className='w-full xl:w-[865px] flex flex-col gap-6'>
-					{/* <Gallery offerId={offerId} /> */}
-					<Tabs defaultValue='description' className='w-full'>
+					<Gallery offerId={offerId} />
+					<Tabs defaultValue='description' className='w-full mb-6'>
 						<TabsList className='bg-white overflow-x-scroll overflow-y-hidden flex justify-start py-6 rounded'>
 							<TabsTrigger className=' text-2xl font-bold' value='description'>
 								Опис
@@ -205,187 +206,199 @@ export default function page({ params }: { params: { offerId: string } }) {
 						</TabsContent>
 					</Tabs>
 				</div>
-				<div className='w-full xl:w-[385px] flex flex-col flex-wrap gap-2 md:gap-6'>
-					<div className='h-36 w-full p-2 bg-white flex flex-col justify-between'>
-						<p
-							className={
-								isAvailable
-									? 'text-green text-lg font-semibold'
-									: 'text-red-900 text-lg font-semibold'
-							}
-						>
-							{isAvailable ? '● В наявності' : '○ Немає в наявності'}
-						</p>
-
-						{currentParams.RetailPriceWithDiscount ===
-						currentParams.RetailPrice ? (
-							<p className='text-black text-[40px] font-semibold'>
-								{formatPrice(currentParams.RetailPrice)} грн.
+				<div className='xl:w-[385px] flex flex-col md:flex-row xl:flex-col gap-2 md:gap-6 '>
+					<div className='flex flex-col gap-6 md:w-1/2 xl:w-full'>
+						<div className='h-36 w-full p-2 bg-white flex flex-col justify-between'>
+							<p
+								className={
+									isAvailable
+										? 'text-green text-lg font-semibold'
+										: 'text-red-900 text-lg font-semibold'
+								}
+							>
+								{isAvailable ? '● В наявності' : '○ Немає в наявності'}
 							</p>
-						) : (
-							<>
-								<p className='line-through text-[20px] font-bold'>
+
+							{currentParams.RetailPriceWithDiscount ===
+							currentParams.RetailPrice ? (
+								<p className='text-black text-[40px] font-semibold'>
 									{formatPrice(currentParams.RetailPrice)} грн.
 								</p>
-								<p className='text-red-900 text-[40px] font-semibold'>
-									{formatPrice(currentParams.RetailPriceWithDiscount)} грн.
-								</p>
-							</>
-						)}
-					</div>
-					<Accord title='Варіанти товару'>
-						<div className='flex flex-col gap-6 text-xs w-full'>
-							<div className='flex gap-2'>
-								<div className='w-[86px] h-[74px] bg-accent rounded' />
-								<div className='text-xs font-semibold'>
-									<div className='flex flex-col w-full items-end'>
-										<h3>Диван Elegant Арен 155x93 см темно-сірий</h3>
-										<p className='text-green'>● В наявності</p>
-										<div>
-											<span className='line-through mr-2'>13 000 грн.</span>
-											<span className='text-red-900 text-[18px]'>
-												12 499 грн.
-											</span>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div className='flex gap-2'>
-								<div className='w-[86px] h-[74px] bg-accent rounded' />
-								<div className='text-xs font-semibold'>
-									<div className='flex flex-col w-full items-end'>
-										<h3>Диван Elegant Арен 155x93 см темно-сірий</h3>
-										<p className='text-green'>● В наявності</p>
-										<div>
-											<span className='line-through mr-2'>13 000 грн.</span>
-											<span className='text-red-900 text-[18px]'>
-												12 499 грн.
-											</span>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div className='flex gap-2'>
-								<div className='w-[86px] h-[74px] bg-accent rounded' />
-								<div className='text-xs font-semibold'>
-									<div className='flex flex-col w-full items-end'>
-										<h3>Диван Elegant Арен 155x93 см темно-сірий</h3>
-										<p className='text-green'>● В наявності</p>
-										<div>
-											<span className='line-through mr-2'>13 000 грн.</span>
-											<span className='text-red-900 text-[18px]'>
-												12 499 грн.
-											</span>
-										</div>
-									</div>
-								</div>
-							</div>
-							{productVariants.length > 0 ? (
-								productVariants.map((variant, index) => (
-									<div key={index} className='flex justify-between text-[16px]'>
-										<span className='flex gap-2 items-center'>
-											<Checkbox
-												id={`variant-${variant.variantId}`}
-												className='border-2 rounded'
-											/>
-											<Label htmlFor={`variant-${variant.variantId}`}>
-												{variant.name}
-											</Label>
-										</span>
-										<span>[{variant.quantity}]</span>
-									</div>
-								))
 							) : (
-								<p className='text-gray-500'>Варианты товара не найдены</p>
+								<>
+									<p className='line-through text-[20px] font-bold'>
+										{formatPrice(currentParams.RetailPrice)} грн.
+									</p>
+									<p className='text-red-900 text-[40px] font-semibold'>
+										{formatPrice(currentParams.RetailPriceWithDiscount)} грн.
+									</p>
+								</>
 							)}
-							<Link href='/' className='text-[#4E3A9F] mt-4'>
-								Згорнути
-							</Link>
 						</div>
-					</Accord>
 
-					<div className='flex justify-between'>
 						<Button
 							onClick={handleAddToCart}
 							className='px-18 py-2 bg-gray hover:bg-gray/90 flex-1'
 						>
 							Купити
 						</Button>
-					</div>
 
-					<Accord title='Наявність в магазинах'>
-						<div className='flex flex-col gap-2 text-xs w-full'>
-							{availableStorages.length > 0 ? (
-								availableStorages.map((storage, index) => (
-									<div
-										key={index}
-										className='flex justify-between items-center text-[16px]'
-									>
-										<span>
-											{storageMap[storage.location as keyof typeof storageMap]}
-										</span>
-										<Check className='text-green' />
+						<Accord title='Варіанти товару'>
+							<div className='flex flex-col gap-6 text-xs w-full'>
+								<div className='flex gap-2'>
+									<div className='w-[86px] h-[74px] bg-accent rounded' />
+									<div className='text-xs font-semibold'>
+										<div className='flex flex-col w-full items-end'>
+											<h3>Диван Elegant Арен 155x93 см темно-сірий</h3>
+											<p className='text-green'>● В наявності</p>
+											<div>
+												<span className='line-through mr-2'>13 000 грн.</span>
+												<span className='text-red-900 text-[18px]'>
+													12 499 грн.
+												</span>
+											</div>
+										</div>
 									</div>
-								))
-							) : (
-								<p className='text-gray-500'>Товару немає на складах</p>
-							)}
-						</div>
-					</Accord>
-					<Accord title='Доставка'>
-						<div className='flex flex-col gap-2 text-xs w-full'>
-							<div className='flex justify-between text-[16px]'>
-								<span className='font-medium'>Самовивіз зі складу:</span>
-								<span className='font-medium text-[#3C9F3A]'> Безкоштовно</span>
-							</div>
-							<p className='text-xs font-semibold'>
-								Безкоштовне самовивезення можливе за наявності товару на цьому
-								складі. Якщо товар знаходиться на віддаленому складі або
-								необхідно переміщення з іншого магазину, вартість погоджується з
-								менеджером
-							</p>
-							<p className='text-xs font-semibold'>
-								При самовивезенні з магазину, винесення та навантаження
-								здійснюється самотужки
-							</p>
-							<div className='flex justify-between text-[16px] font-medium'>
-								<span>Доставка по Києву</span>
-								<span>599 грн</span>
-							</div>
-							<div className='flex justify-between text-[16px] font-medium'>
-								<span>По Україні від:</span>
-								<span>599 грн</span>
-							</div>
-						</div>
-					</Accord>
-					<Accord title='Оплата'>
-						<div className='flex flex-col gap-2 text-xs'>
-							<p className='font-semibold'>
-								Готівкою або через термінал у магазині, безготівковий
-								розрахунок, карткою, розстрочка, оплата частинами, Whitepay
-								crypto
-							</p>
-							<Link href='/' className='text-[#4E3A9F] font-semibold'>
-								Дізнатися всі умови
-							</Link>
-						</div>
-					</Accord>
+								</div>
 
-					<Accord title='Гарантія та повернення'>
-						<div className='flex flex-col gap-2 text-xs'>
-							<p className='font-semibold'>
-								Гарантія від виробника до 2 років до 24 місяців з моменту
-								покупки. Гарантійний термін починається з доставки виробу
-								клієнту додому. 14 днів з наступного дня після доставки виробу
-								додому ви маєте право обміняти або повернути куплений товар.
-							</p>
-							<Link href='/' className='text-[#4E3A9F] font-semibold'>
-								Дізнатися всі умови
-							</Link>
-						</div>
-					</Accord>
+								<div className='flex gap-2'>
+									<div className='w-[86px] h-[74px] bg-accent rounded' />
+									<div className='text-xs font-semibold'>
+										<div className='flex flex-col w-full items-end'>
+											<h3>Диван Elegant Арен 155x93 см темно-сірий</h3>
+											<p className='text-green'>● В наявності</p>
+											<div>
+												<span className='line-through mr-2'>13 000 грн.</span>
+												<span className='text-red-900 text-[18px]'>
+													12 499 грн.
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className='flex gap-2'>
+									<div className='w-[86px] h-[74px] bg-accent rounded' />
+									<div className='text-xs font-semibold'>
+										<div className='flex flex-col w-full items-end'>
+											<h3>Диван Elegant Арен 155x93 см темно-сірий</h3>
+											<p className='text-green'>● В наявності</p>
+											<div>
+												<span className='line-through mr-2'>13 000 грн.</span>
+												<span className='text-red-900 text-[18px]'>
+													12 499 грн.
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+								{productVariants.length > 0 ? (
+									productVariants.map((variant, index) => (
+										<div
+											key={index}
+											className='flex justify-between text-[16px]'
+										>
+											<span className='flex gap-2 items-center'>
+												<Checkbox
+													id={`variant-${variant.variantId}`}
+													className='border-2 rounded'
+												/>
+												<Label htmlFor={`variant-${variant.variantId}`}>
+													{variant.name}
+												</Label>
+											</span>
+											<span>[{variant.quantity}]</span>
+										</div>
+									))
+								) : (
+									<p className='text-gray-500'>Варианты товара не найдены</p>
+								)}
+								<Link href='/' className='text-[#4E3A9F] mt-4'>
+									Згорнути
+								</Link>
+							</div>
+						</Accord>
+
+						<Accord title='Наявність в магазинах'>
+							<div className='flex flex-col gap-2 text-xs w-full'>
+								{availableStorages.length > 0 ? (
+									availableStorages.map((storage, index) => (
+										<div
+											key={index}
+											className='flex justify-between items-center text-[16px]'
+										>
+											<span>
+												{
+													storageMap[
+														storage.location as keyof typeof storageMap
+													]
+												}
+											</span>
+											<Check className='text-green' />
+										</div>
+									))
+								) : (
+									<p className='text-gray-500'>Товару немає на складах</p>
+								)}
+							</div>
+						</Accord>
+					</div>
+					<div className='flex flex-col gap-6 md:w-1/2 xl:w-full'>
+						<Accord title='Доставка'>
+							<div className='flex flex-col gap-2 text-xs w-full'>
+								<div className='flex justify-between text-[16px]'>
+									<span className='font-medium'>Самовивіз зі складу:</span>
+									<span className='font-medium text-[#3C9F3A]'>
+										{' '}
+										Безкоштовно
+									</span>
+								</div>
+								<p className='text-xs font-semibold'>
+									Безкоштовне самовивезення можливе за наявності товару на цьому
+									складі. Якщо товар знаходиться на віддаленому складі або
+									необхідно переміщення з іншого магазину, вартість погоджується
+									з менеджером
+								</p>
+								<p className='text-xs font-semibold'>
+									При самовивезенні з магазину, винесення та навантаження
+									здійснюється самотужки
+								</p>
+								<div className='flex justify-between text-[16px] font-medium'>
+									<span>Доставка по Києву</span>
+									<span>599 грн</span>
+								</div>
+								<div className='flex justify-between text-[16px] font-medium'>
+									<span>По Україні від:</span>
+									<span>599 грн</span>
+								</div>
+							</div>
+						</Accord>
+						<Accord title='Оплата'>
+							<div className='flex flex-col gap-2 text-xs'>
+								<p className='font-semibold'>
+									Готівкою або через термінал у магазині, безготівковий
+									розрахунок, карткою, розстрочка, оплата частинами, Whitepay
+									crypto
+								</p>
+								<Link href='/' className='text-[#4E3A9F] font-semibold'>
+									Дізнатися всі умови
+								</Link>
+							</div>
+						</Accord>
+						<Accord title='Гарантія та повернення'>
+							<div className='flex flex-col gap-2 text-xs'>
+								<p className='font-semibold'>
+									Гарантія від виробника до 2 років до 24 місяців з моменту
+									покупки. Гарантійний термін починається з доставки виробу
+									клієнту додому. 14 днів з наступного дня після доставки виробу
+									додому ви маєте право обміняти або повернути куплений товар.
+								</p>
+								<Link href='/' className='text-[#4E3A9F] font-semibold'>
+									Дізнатися всі умови
+								</Link>
+							</div>
+						</Accord>
+					</div>
 				</div>
 			</div>
 		</>
